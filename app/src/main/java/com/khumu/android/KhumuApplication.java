ba3399e85f6576533013d665beab1e96f07460b2
Jinsu Park <bo314@naver.com>
2020-11-26 22:18:18 +0900

    Add: Draft simple FCM Push

--- app/src/main/java/com/khumu/android/KhumuApplication.java
+++ app/src/main/java/com/khumu/android/KhumuApplication.java
@@ -3,7 +3,13 @@ package com.khumu.android;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.khumu.android.repository.ArticleRepository;
 import com.khumu.android.repository.CommentRepository;
 import com.khumu.android.repository.LikeArticleRepository;
@@ -14,6 +20,7 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 
 public class KhumuApplication extends Application {
+    private final static String TAG = "KhumuApplication";
     public static String username;
     public static String nickname;
     public static String token;
@@ -26,12 +33,27 @@ public class KhumuApplication extends Application {
         // 우리의 필요한 의존성들을 이 container에 Singleton으로 관리
         Util.init();
         container = DaggerContainer.create();
-        Context context = getApplicationContext();
         sharedPref = getSharedPreferences(
             getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
         // sharedPreferences를 이용해 필요한 데이터 초기화.
         loadKhumuConfig();
+
+        // FCM Push를 위해 초기화함.
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        FirebaseMessaging.getInstance().getToken()
+            .addOnCompleteListener(new OnCompleteListener<String>() {
+                @Override
+                public void onComplete(@NonNull Task<String> task) {
+                    if (task.isSuccessful()) {// Get new FCM registration token
+                        String token = task.getResult();
+                        Log.d(TAG, "FCM Token: " + token);
+                    } else {
+                        Log.d(TAG, "Fetching FCM registration token failed", task.getException());
+                        return;
+                    }
+                }
+            });
     }
     public static void loadKhumuConfig(){
         username = sharedPref.getString("username", null);
