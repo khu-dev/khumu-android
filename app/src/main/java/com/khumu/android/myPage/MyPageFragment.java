8a6ec246260cce8b267c14142f7bae5073168626
Jinsu Park <bo314@naver.com>
2021-01-21 03:00:50 +0900

    Refactoring: MyPage에서 onClickListener들도 DataBinding으로

--- app/src/main/java/com/khumu/android/myPage/MyPageFragment.java
+++ app/src/main/java/com/khumu/android/myPage/MyPageFragment.java
@@ -34,16 +34,8 @@ import java.util.List;
 
 public class MyPageFragment extends Fragment {
     private final static String TAG = "MyPageFragment";
+    private FragmentMyPageBinding fragmentMyPageBinding;
     private MyPageViewModel myPageViewModel;
-
-    private TextView usernameTV;
-    private TextView nicknameTV;
-    private TextView articlesWrittenTV;
-    private TextView articlesLikedTV;
-    private TextView articlesBookmarkedTV;
-    private TextView articlesCommentedTV;
-    private TextView logoutTV;
-    private RecyclerView followingArticleTagsRecyclerView;
     private FlexboxLayoutManager followingArticleTagsLayoutManager;
     private ArticleTagAdapter followingArticleTagAdapter;
 
@@ -77,16 +69,11 @@ public class MyPageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        // 아직 ViewModel은 안 다룸.
-        // homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        // 나의 부모인 컨테이너에서 내가 그리고자 하는 녀석을 얻어옴. 사실상 루트로 사용할 애를 객체와.
-        // inflate란 xml => java 객체
-//        View root = inflater.inflate(R.layout.fragment_my_page, container, false);
-
-        FragmentMyPageBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false);
-        View root = binding.getRoot();
-        // binding하며 사용할 Fragment가 사용하는 변수인 viewModel을 설정해줌.
-        binding.setViewModel(myPageViewModel);
+        fragmentMyPageBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false);
+        View root = fragmentMyPageBinding.getRoot();
+        // binding하며 사용할 Fragment가 사용하는 변수들을 주입해준다.
+        fragmentMyPageBinding.setViewModel(myPageViewModel);
+        fragmentMyPageBinding.setFragment(this);
         return root;
     }
 
@@ -94,32 +81,16 @@ public class MyPageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //view는 root 즉 fragment
-
-        // root.findViewById(R.id.action_navigation_board_to_navigation_home).setOnClickListener(Navigation);
-        // xml 상에 recyclerview는 실질적으로 아이템이 어떻게 구현되어있는지 정의되어있지 않다.
-        // linearlayout의 형태를 이용하겠다면 linearlayoutmanager을 이용한다.
         findViews(view);
-//        setAdapters();
-//        setEventListeners(view);
     }
 
     private void findViews(View root){
-        usernameTV = root.findViewById(R.id.my_page_profile_username_tv);
-        nicknameTV = root.findViewById(R.id.my_page_profile_nickname_tv);
-        articlesWrittenTV = root.findViewById(R.id.my_page_articles_written_tv);
-        articlesLikedTV = root.findViewById(R.id.my_page_articles_liked_tv);
-        articlesBookmarkedTV = root.findViewById(R.id.my_page_articles_bookmarked_tv);
-        articlesCommentedTV = root.findViewById(R.id.my_page_articles_commented_tv);
-        logoutTV = root.findViewById(R.id.my_page_logout_tv);
-
-//        usernameTV.setText(KhumuApplication.getUsername());
-//        nicknameTV.setText(KhumuApplication.getNickname());
-
-        followingArticleTagsRecyclerView = root.findViewById(R.id.my_page_following_article_tags_recycler_view);
         followingArticleTagsLayoutManager = new FlexboxLayoutManager(this.getContext());
         followingArticleTagsLayoutManager.setFlexDirection(FlexDirection.ROW); // 기본값임
         followingArticleTagsLayoutManager.setJustifyContent(JustifyContent.FLEX_START); // 기본값임
-        followingArticleTagsRecyclerView.setLayoutManager(followingArticleTagsLayoutManager);
+        fragmentMyPageBinding.myPageFollowingArticleTagsRecyclerView.setLayoutManager(followingArticleTagsLayoutManager);
+
+        // dummy tags
         List<ArticleTag> l = new ArrayList<>();
         l.add(new ArticleTag("hello", true));
         l.add(new ArticleTag("world", true));
@@ -138,69 +109,51 @@ public class MyPageFragment extends Fragment {
         l.add(new ArticleTag("hello", true));
         l.add(new ArticleTag("world", true));
 
-
         followingArticleTagAdapter = new ArticleTagAdapter(l);
-        followingArticleTagsRecyclerView.setAdapter(followingArticleTagAdapter);
+        fragmentMyPageBinding.myPageFollowingArticleTagsRecyclerView.setAdapter(followingArticleTagAdapter);
+    }
 
-        articlesWrittenTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyPageFragment.this.getActivity(), SingleBoardFeedActivity.class);
-                Board board = new Board("my", null, "작성한 게시물", "본인이 작성한 게시물들입니다.", false, null, null);
-                intent.putExtra("board", board);
-                startActivity(intent);
-            }
-        });
+    public void onClickArticlesWrittenByMeTV(View v){
+        startSingleBoardFeedActivity("my", null, "작성한 게시물", "본인이 작성한 게시물들입니다.");
+    }
 
-        articlesLikedTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyPageFragment.this.getActivity(), SingleBoardFeedActivity.class);
-                Board board = new Board("liked", null, "좋아요한 게시물", "좋아요를 누른 게시물들입니다.", false, null, null);
-                intent.putExtra("board", board);
-                startActivity(intent);
-            }
-        });
+    public void onClickArticlesLikedTV(View v){
+        startSingleBoardFeedActivity("liked", null,"좋아요한 게시물", "좋아요를 누른 게시물들입니다.");
+    }
+    public void onClickArticlesBookmarkedTV(View v){
+        startSingleBoardFeedActivity("bookmarked", null, "북마크한 게시물", "북마크해놓은 게시물들입니다.");
+    }
+    public void onClickArticlesCommentedTV(View v){
+        startSingleBoardFeedActivity("commented", null, "댓글단 게시물", "댓글을 작성했던 게시물들입니다.");
+    }
 
-        articlesBookmarkedTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyPageFragment.this.getActivity(), SingleBoardFeedActivity.class);
-                Board board = new Board("bookmarked", null, "북마크한 게시물", "북마크해놓은 게시물들입니다.", false, null, null);
-                intent.putExtra("board", board);
-                startActivity(intent);
-            }
-        });
+    /**
+     * SingleBoard에 대한 Feed를 보여주는 Activity인 SingleBoardFeedActivity를 시작한다.
+     * 주로 MyPage에서 내가 작성한 게시물이나 좋아요한 게시물 등 논리적 Board에 대한 Feed를 보고자 할 때 사용된다.
+     */
+    protected void startSingleBoardFeedActivity(String boardName, String boardCategory, String boardDisplayName, String boardDescription) {
+        Intent intent = new Intent(MyPageFragment.this.getActivity(), SingleBoardFeedActivity.class);
+        Board board = new Board(boardName, boardCategory, boardDisplayName, boardDescription, false, null, null);
+        intent.putExtra("board", board);
+        startActivity(intent);
+    }
 
-        articlesCommentedTV.setOnClickListener(new View.OnClickListener() {
+    public void onClickLogoutTV(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+        builder.setTitle("로그아웃하시겠습니까?").setMessage("로그아웃 후 쿠뮤가 종료됩니다.").setCancelable(true).setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyPageFragment.this.getActivity(), SingleBoardFeedActivity.class);
-                Board board = new Board("commented", null, "댓글단 게시물", "댓글을 작성했던 게시물들입니다.", false, null, null);
-                intent.putExtra("board", board);
-                startActivity(intent);
+            public void onClick(DialogInterface dialog, int which) {
+                KhumuApplication.clearKhumuAuthenticationConfig();
+                KhumuApplication.loadKhumuConfig(); // refresh 과정
+                MyPageFragment.this.getActivity().finishAffinity();
+            }
+        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
             }
         });
-        logoutTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(root.getContext());
-                builder.setTitle("로그아웃하시겠습니까?").setMessage("로그아웃 후 쿠뮤가 종료됩니다.").setCancelable(true).setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        KhumuApplication.clearKhumuAuthenticationConfig();
-                        KhumuApplication.loadKhumuConfig(); // refresh 과정
-                        MyPageFragment.this.getActivity().finishAffinity();
-                    }
-                }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        return;
-                    }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
-        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
