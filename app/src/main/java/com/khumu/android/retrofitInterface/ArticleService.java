9da5319cd7357a009c3753f1da93d7dc8446c6bc
gusrl4025 <gusrl4025@naver.com>
2021-01-26 16:37:09 +0900

    a

--- app/src/main/java/com/khumu/android/retrofitInterface/ArticleService.java
+++ app/src/main/java/com/khumu/android/retrofitInterface/ArticleService.java
@@ -1,14 +1,27 @@
 package com.khumu.android.retrofitInterface;
 
 import com.khumu.android.data.ArticleListResponse;
+import com.khumu.android.data.Result;
+import com.khumu.android.data.article.Article;
+
+import java.util.HashMap;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.FieldMap;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ArticleService {
     @GET("articles")
     Call<ArticleListResponse> getArticles(@Header("Authorization") String authorization, @Query("page") String page);
+
+    @GET("articles")
+    Call<ArticleListResponse> getArticles(@Header("Authorization") String authorization, @Query("page") String page, @Query("board") String board);
+
+    @POST("articles")
+    Call<Article> createArticle(@Header("Authorization") String authorization, @Body Article article);
 }
 
