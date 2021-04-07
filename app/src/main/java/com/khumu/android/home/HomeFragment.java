c9a0849bce4ab12762f483c95c3a99cf84a1c519
Jinsu Park <bo314@naver.com>
2021-04-07 14:58:54 +0900

    Draft: 웹뷰 PoC

--- app/src/main/java/com/khumu/android/home/HomeFragment.java
+++ app/src/main/java/com/khumu/android/home/HomeFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,34 +34,13 @@ import java.util.List;
 import javax.inject.Inject;
 
 public class HomeFragment extends Fragment {
-    @Inject
-    public BoardRepository br;
-    @Inject
-    public NotificationService ns;
-    private RecentArticleAdapter recentArticleAdapter;
-    private ListView recentArticlesListView;
-    private HomeViewModel homeViewModel;
-    private Toolbar toolbar;
-    private TextView notificationCountTV;
-    ImageView qrIconIV;
+    WebView webView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         // Layout inflate 이전
         // savedInstanceState을 이용해 다룰 데이터가 있으면 다룸.
-        KhumuApplication.applicationComponent.inject(this);
         super.onCreate(savedInstanceState);
-        homeViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
-            @NonNull
-            @Override
-            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-                return (T) new HomeViewModel(br, ns);
-            }
-        }).get(HomeViewModel.class);
-
-        recentArticleAdapter = new RecentArticleAdapter(
-                getContext(),
-                R.layout.layout_home_recent_article_item,
-                new ArrayList<Article>());
+        KhumuApplication.applicationComponent.inject(this);
     }
 
     @Override
@@ -67,56 +49,47 @@ public class HomeFragment extends Fragment {
         // 나의 부모인 컨테이너에서 내가 그리고자 하는 녀석을 얻어옴. 사실상 루트로 사용할 애를 객체와.
         // inflate란 xml => java 객체
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
+        // status bar 색상
+        // refs: https://stackoverflow.com/a/56433156/9471220
+        this.getActivity().getWindow().setStatusBarColor(this.getActivity().getColor(R.color.red_500));
+        this.getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                this.getActivity().getWindow().getDecorView().getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark\
         return root;
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        toolbar = view.findViewById(R.id.toolbar);
-        notificationCountTV = view.findViewById(R.id.notification_count_tv);
-        recentArticlesListView = view.findViewById(R.id.recent_articles_list);
-        recentArticlesListView.setAdapter(recentArticleAdapter);
-        homeViewModel.getLiveDataRecentArticles().observe(getViewLifecycleOwner(), new Observer<List<Article>>() {
-            @Override
-            public void onChanged(List<Article> articles) {
-                for(Article a : articles){
-
-                    recentArticleAdapter.add(a);
-                }
-                recentArticleAdapter.notifyDataSetChanged();
-            }
-        });
-
-        homeViewModel.getNotifications().observe(getViewLifecycleOwner(), notifications -> {
-            notificationCountTV.setText(String.valueOf(
-                    notifications.stream().filter(
-                            n->!n.isRead()
-                    ).count()));
-        });
-        qrIconIV = view.findViewById(R.id.qr_icon_iv);
-        qrIconIV.setImageResource(R.drawable.ic_qr);
-
-        setEventListeners();
-    }
-
-    public void setEventListeners() {
-        toolbar.setOnClickListener(l->{
-            Intent intent = new Intent(HomeFragment.this.getActivity(), NotificationActivity.class);
-            startActivity(intent);
-        });
-
-        qrIconIV.setOnClickListener(v->{
-            Intent intent = new Intent(this.getActivity(), QrCodeActivity.class);
-            startActivity(intent);
-        });
+        webView = view.findViewById(R.id.home_web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://umi0410.github.io");
+        // client가 없으면 그냥 기본 브라우저 사용.
+        webView.setWebViewClient(new WebViewClient());
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        // notification 읽은 뒤 다시 list해서 is_read 처리가 변경된 list를 얻어오게 list
-        homeViewModel.listNotifications();
+    public void onDestroyView() {
+        super.onDestroyView();
+        this.getActivity().getWindow().setStatusBarColor(this.getActivity().getColor(R.color.white));
+        this.getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
     }
+    //    public void setEventListeners() {
+//        toolbar.setOnClickListener(l->{
+//            Intent intent = new Intent(HomeFragment.this.getActivity(), NotificationActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        qrIconIV.setOnClickListener(v->{
+//            Intent intent = new Intent(this.getActivity(), QrCodeActivity.class);
+//            startActivity(intent);
+//        });
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        // notification 읽은 뒤 다시 list해서 is_read 처리가 변경된 list를 얻어오게 list
+//        homeViewModel.listNotifications();
+//    }
 }
---
+++ app/src/main/java/com/khumu/android/home/OldHomeFragment.java
@@ -0,0 +1,122 @@
+package com.khumu.android.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.khumu.android.KhumuApplication;
+import com.khumu.android.R;
+import com.khumu.android.data.Article;
+import com.khumu.android.notifications.NotificationActivity;
+import com.khumu.android.qrCode.QrCodeActivity;
+import com.khumu.android.repository.BoardRepository;
+import com.khumu.android.repository.NotificationService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class OldHomeFragment extends Fragment {
+    @Inject
+    public BoardRepository br;
+    @Inject
+    public NotificationService ns;
+    private RecentArticleAdapter recentArticleAdapter;
+    private ListView recentArticlesListView;
+    private HomeViewModel homeViewModel;
+    private Toolbar toolbar;
+    private TextView notificationCountTV;
+    ImageView qrIconIV;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        // Layout inflate 이전
+        // savedInstanceState을 이용해 다룰 데이터가 있으면 다룸.
+        KhumuApplication.applicationComponent.inject(this);
+        super.onCreate(savedInstanceState);
+        homeViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new HomeViewModel(br, ns);
+            }
+        }).get(HomeViewModel.class);
+
+        recentArticleAdapter = new RecentArticleAdapter(
+                getContext(),
+                R.layout.layout_home_recent_article_item,
+                new ArrayList<Article>());
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // 나의 부모인 컨테이너에서 내가 그리고자 하는 녀석을 얻어옴. 사실상 루트로 사용할 애를 객체와.
+        // inflate란 xml => java 객체
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar = view.findViewById(R.id.toolbar);
+        notificationCountTV = view.findViewById(R.id.notification_count_tv);
+        recentArticlesListView = view.findViewById(R.id.recent_articles_list);
+        recentArticlesListView.setAdapter(recentArticleAdapter);
+        homeViewModel.getLiveDataRecentArticles().observe(getViewLifecycleOwner(), new Observer<List<Article>>() {
+            @Override
+            public void onChanged(List<Article> articles) {
+                for(Article a : articles){
+
+                    recentArticleAdapter.add(a);
+                }
+                recentArticleAdapter.notifyDataSetChanged();
+            }
+        });
+
+        homeViewModel.getNotifications().observe(getViewLifecycleOwner(), notifications -> {
+            notificationCountTV.setText(String.valueOf(
+                    notifications.stream().filter(
+                            n->!n.isRead()
+                    ).count()));
+        });
+        qrIconIV = view.findViewById(R.id.qr_icon_iv);
+        qrIconIV.setImageResource(R.drawable.ic_qr);
+
+        setEventListeners();
+    }
+
+    public void setEventListeners() {
+        toolbar.setOnClickListener(l->{
+            Intent intent = new Intent(OldHomeFragment.this.getActivity(), NotificationActivity.class);
+            startActivity(intent);
+        });
+
+        qrIconIV.setOnClickListener(v->{
+            Intent intent = new Intent(this.getActivity(), QrCodeActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // notification 읽은 뒤 다시 list해서 is_read 처리가 변경된 list를 얻어오게 list
+        homeViewModel.listNotifications();
+    }
+}
