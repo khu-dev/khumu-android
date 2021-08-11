3e01e6d4fa2b160fdb22463c674b05c51a4ec6a1
Jinsu Park <dev.umijs@gmail.com>
2021-08-11 17:25:08 +0900

    feat: 오픈소스 웹뷰를 이용해 url 표시 및 공유 기능

--- app/src/main/java/com/khumu/android/adapter/SimpleAnnouncementAdapter.java
+++ app/src/main/java/com/khumu/android/adapter/SimpleAnnouncementAdapter.java
@@ -1,28 +1,21 @@
 package com.khumu.android.adapter;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.khumu.android.KhumuApplication;
 import com.khumu.android.R;
-import com.khumu.android.WebViewActivity;
 import com.khumu.android.data.Announcement;
 import com.khumu.android.databinding.LayoutSimpleAnnouncementItemBinding;
-import java.util.List;
+import com.thefinestartist.finestwebview.FinestWebView;
 
-import javax.inject.Inject;
+import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -60,9 +53,12 @@ public class SimpleAnnouncementAdapter extends RecyclerView.Adapter<SimpleAnnoun
         Announcement announcement = announcementList.get(position);
         holder.bind(announcement);
         holder.binding.getRoot().setOnClickListener(v -> {
-            Intent intent = new Intent(context, WebViewActivity.class);
-            intent.putExtra("url", announcement.getReferenceUrl());
-            context.startActivity(intent);
+            new FinestWebView.Builder(context)
+                    .titleColor(ContextCompat.getColor(context, R.color.white))
+                    .show(announcement.getReferenceUrl());
+//            Intent intent = new Intent(context, WebViewActivity.class);
+//            intent.putExtra("url", announcement.getReferenceUrl());
+//            context.startActivity(intent);
         });
     }
 
