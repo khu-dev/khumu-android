3bfb2a13b229c2754555ef84b240fe98b9d38611
Jinsu Park <dev.umijs@gmail.com>
2022-03-09 16:51:12 +0900

    feat: commnitiy API endpoint

--- app/src/main/java/com/khumu/android/repository/ArticleService.java
+++ app/src/main/java/com/khumu/android/repository/ArticleService.java
@@ -18,30 +18,30 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ArticleService {
-    @GET("articles")
+    @GET("community/v1/articles")
     Call<ArticleListResponse> getArticles(@Query("page") int page);
 
-    @GET("articles")
+    @GET("community/v1/articles")
     Call<ArticleListResponse> getArticles(@Query("board") String board, @Query("page") int page, @Query("size") int size);
 
-    @GET("articles")
+    @GET("community/v1/articles")
     Call<ArticleListResponse> searchArticles(@Query("q") String q, @Nullable @Query("cursor") String cursor);
 
-    @GET("articles/{id}")
+    @GET("community/v1/articles/{id}")
     Call<ArticleResponse> getArticle(@Path("id") int id);
 
-    @POST("articles")
+    @POST("community/v1/articles")
     Call<Article> createArticle(@Header("Content-Type") String contentType, @Body Article article);
 
-    @PATCH("articles/{id}")
+    @PATCH("community/v1/articles/{id}")
     Call<Article> updateArticle(@Header("Content-Type") String contentType, @Path("id") int id, @Body Article article);
 
-    @DELETE("articles/{id}")
+    @DELETE("community/v1/articles/{id}")
     Call<Void> deleteArticle(@Header("Content-Type") String contentType, @Path("id") int id);
 
-    @PATCH("articles/{id}/likes")
+    @PATCH("community/v1/articles/{id}/likes")
     Call<DefaultResponse> likeArticleToggle(@Header("Content-Type") String contentType, @Path("id") int id);
-    @PATCH("articles/{id}/bookmarks")
+    @PATCH("community/v1/articles/{id}/bookmarks")
     Call<DefaultResponse> bookmarkArticleToggle(@Header("Content-Type") String contentType, @Path("id") int id);
 
 
