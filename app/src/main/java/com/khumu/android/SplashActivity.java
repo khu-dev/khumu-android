e9fdff7287896aec0168b9a1c5190bf24fc3506d
dizzi <gusrl4025@gmail.com>
2022-04-03 22:54:40 +0900

    feat: Splash페이지 임시 추가

--- app/src/main/java/com/khumu/android/SplashActivity.java
+++ app/src/main/java/com/khumu/android/SplashActivity.java
@@ -1,31 +1,38 @@
-//package com.khumu.android;
+package com.khumu.android;//package com.khumu.android;
 //
 //import android.content.Intent;
 //import android.os.Bundle;
-//import android.os.Handler;
-//import android.view.KeyEvent;
-//import android.view.View;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.content.ContextCompat;
-//import androidx.fragment.app.Fragment;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.NavigationUI;
-//
-//import com.google.android.material.bottomnavigation.BottomNavigationView;
-//import com.khumu.android.home.HomeFragment;
-//import com.khumu.android.login.LoginActivity;
-//
-//public class SplashActivity extends AppCompatActivity{
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_splash);
-//
-//        Handler handler = new Handler();
-//        handler.postDelayed(() -> {
-//
-//        }, 500L);
-//    }
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.khumu.android.MainActivity;
+import com.khumu.android.home.HomeFragment;
+import com.khumu.android.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000L);
+
+
+    }
+}
